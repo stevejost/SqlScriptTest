8f4f39873a6bee4f412287dcafec6ca7849fa5d7
Steve Jost <steve@stevejost.com>
2016-04-07 01:48:21 -0700

    SQL Scripter Test

---
+++ ConsoleApplication7/CommandLineOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace ConsoleApplication7
+{
+    public class CommandLineOptions
+    {
+        [Option('d',"database", Required = true)]
+        public string Database { get; set; }
+
+        [Option('s',"server", Required = true)]
+        public string Server { get; set; }
+    }
+}
--- ConsoleApplication7/Program.cs
+++ ConsoleApplication7/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Sdk.Sfc;
+using Microsoft.SqlServer.Management.Smo;
 
 namespace ConsoleApplication7
 {
@@ -10,6 +14,109 @@ namespace ConsoleApplication7
     {
         static void Main(string[] args)
         {
+            var opts = new CommandLineOptions();
+            var result = CommandLine.Parser.Default.ParseArguments(args, opts);
+
+            var dbName = opts.Database;
+            var srv = new Server(new ServerConnection(opts.Server));
+
+            var db = srv.Databases[dbName];
+
+            Scripter script = new Scripter(srv);
+            Scripter roleScripter = new Scripter(srv);
+
+            script.Options.ScriptDrops = false;            
+            script.Options.Indexes = true;              // To include indexes
+            script.Options.DriAllConstraints = true;   // to include referential constraints in the script
+            script.Options.Permissions = true;
+            script.Options.TargetServerVersion = SqlServerVersion.Version105;
+            script.Options.Triggers = true;
+            
+            
+            foreach (DatabaseRole role in db.Roles)
+            {
+                if(!role.IsFixedRole) { 
+                Console.WriteLine("-- Scripting for role " + role.Name);
+
+                // Generating script for table tb
+                System.Collections.Specialized.StringCollection sc = roleScripter.Script(new Urn[] { role.Urn });
+                foreach (string st in sc)
+                {
+                    Console.WriteLine(st);
+                }
+                Console.WriteLine("--");
+                    Console.WriteLine("GO");
+                }
+            }
+
+            foreach (Table tb in db.Tables)
+            {
+                if (tb.IsSystemObject == false)
+                {
+                    Console.WriteLine("-- Scripting for table " + tb.Name);
+
+                    // Generating script for table tb
+                    System.Collections.Specialized.StringCollection sc = script.Script(new Urn[] { tb.Urn });
+                    foreach (string st in sc)
+                    {
+                        Console.WriteLine(st);
+                    }
+                    Console.WriteLine("--");
+                    Console.WriteLine("GO");
+                }
+            }
+
+            foreach (View tb in db.Views)
+            {
+                if (tb.IsSystemObject == false)
+                {
+                    Console.WriteLine("-- Scripting for View " + tb.Name);
+
+                    // Generating script for table tb
+                    System.Collections.Specialized.StringCollection sc = script.Script(new Urn[] { tb.Urn });
+                    foreach (string st in sc)
+                    {
+                        Console.WriteLine(st);
+                    }
+                    Console.WriteLine("--");
+                }
+            }
+
+            foreach (StoredProcedure tb in db.StoredProcedures)
+            {
+                if (tb.IsSystemObject == false)
+                {
+                    Console.WriteLine("-- Scripting for Stored Procedure " + tb.Name);
+
+                    // Generating script for table tb
+                    System.Collections.Specialized.StringCollection sc = script.Script(new Urn[] { tb.Urn });
+                    foreach (string st in sc)
+                    {
+                        Console.WriteLine(st);
+                    }
+                    Console.WriteLine("--");
+                    Console.WriteLine("GO");
+                }
+            }
+
+            foreach (UserDefinedFunction udf in db.UserDefinedFunctions)
+            {
+                if (udf.IsSystemObject == false)
+                {
+                    Console.WriteLine("-- Scripting for Stored Procedure " + udf.Name);
+
+                    // Generating script for table tb
+                    System.Collections.Specialized.StringCollection sc = script.Script(new Urn[] { udf.Urn });
+                    foreach (string st in sc)
+                    {
+                        Console.WriteLine(st);
+                    }
+                    Console.WriteLine("--");
+                    Console.WriteLine("GO");
+                }
+            }
+
+            
         }
     }
 }
